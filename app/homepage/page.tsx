dcf04183f710c6b6af24c529e306123eeb98df44
severogianluca <gianlucasevero300@gmail.com>
2025-12-15 17:15:31 +0100

    Feat: add section News

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { FaClock, FaUser } from "react-icons/fa";
 
 
 interface HoverLink {
@@ -25,7 +26,7 @@ function Home() {
     return (
         <>
             <div className="container pt-3">
-                <div className="d-flex d-flex justify-content-between align-items-center">
+                <div className="d-flex justify-content-between align-items-center">
                     <div className="titolo">
                         <h1 className={visible ? "visible" : ""}>Creiamo tecnologie personalizzate che accelerano la tua <span className="text-yellow">produttività aziendale</span></h1>
                     </div>
@@ -33,9 +34,18 @@ function Home() {
                         <Image src="/photo1.png" width={500} height={500} alt="foto di grupp" />
                     </div>
                 </div>
+                <div className="d-flex" style={{ paddingRight: "80px"}}>
+                    <Image
+                        src="/photo7.png"
+                        width={400}
+                        height={200}
+                        alt="partner"
+                        className="ms-auto"
+                    />
+                </div>
 
 
-                <div className="d-flex d-flex justify-content-between align-items-center gap-3 pt-5">
+                <div className="d-flex justify-content-between align-items-center gap-3 pt-5 ">
                     <div className="px-5">
                         <Image src="/photo2.jpg" width={300} height={340} alt="foto di gruppo" className="foto-tondo" />
                     </div>
@@ -55,12 +65,12 @@ function Home() {
                 </div>
 
 
-                <div className="d-flex d-flex justify-content-between align-items-center gap-3 pt-5 mt-5">
+                <div className="d-flex justify-content-between align-items-center gap-3 pt-5 mt-5 pb-5">
                     <div className="titolo">
                         <p style={{ color: "#b3b3b3" }}>COSA FACCIAMO</p>
                         <h2 className="pb-5">Cosa possiamo fare per  <span className="text-yellow">la tua azienda</span></h2>
                         <div className="d-flex justify-content-center w-100 mt-3">
-                            <Image src="/photo3.png" width={300} height={300} alt="foto puzzle"></Image>
+                            <Image src="/photo3.png" width={300} height={300} alt="foto puzzle" />
                         </div>
                     </div>
                     <div className="ps-5">
@@ -74,7 +84,91 @@ function Home() {
                         ))}
                     </div>
                 </div>
-                
+
+                <div>
+                    <h1 className="text-yellow pt-5 mt-5">News</h1>
+
+                    <div
+                        className="d-flex gap-3 pt-3"
+                        style={{ alignItems: "stretch" }}
+                    >
+
+                        {/* IMG SX */}
+                        <div style={{ position: "relative", width: "700px", height: "480px" }}>
+                            <Image
+                                src="/photo4.jpg"
+                                alt="foto"
+                                fill
+                                style={{ objectFit: "cover" }}
+                            />
+                            {/* TESTO IMG SX */}
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    bottom: "20px",
+                                    left: "20px",
+                                    color: "white",
+                                    background: "rgba(0,0,0,0.5)",
+                                    padding: "12px 16px",
+                                    borderRadius: "8px",
+                                    maxWidth: "80%",
+                                }}
+                            >
+                                <div className="d-flex align-items-center gap-2">
+                                    <FaUser size={16} style={{ color: "#b3b3b3" }} />
+                                    <span className="m-0" style={{ color: "#b3b3b3" }}>Arianna Salpietro on</span>
+                                    <FaClock size={14} style={{ color: "#b3b3b3" }} />
+                                    <span style={{ color: "#b3b3b3" }}>18 Novembre 2025</span>
+                                </div>
+                                <p className="m-0" style={{ color: "#b3b3b3" }}>XCC sponsor del Public Sector Innovation Day</p>
+                            </div>
+                        </div>
+
+                        {/* IMG DX */}
+                        <div
+                            className="d-flex flex-column gap-3"
+                            style={{ height: "480px", width: "400px" }}
+                        >
+                            <div style={{ position: "relative", flex: 1 }}>
+                                <Image
+                                    src="/photo5.jpeg"
+                                    alt="foto"
+                                    fill
+                                    style={{ objectFit: "cover" }}
+                                />
+                                {/* TESTO IMG DX */}
+                                <div
+                                    style={{
+                                        position: "absolute",
+                                        bottom: "20px",
+                                        left: "20px",
+                                        color: "white",
+                                        background: "rgba(0,0,0,0.5)",
+                                        padding: "12px 16px",
+                                        borderRadius: "8px",
+                                        maxWidth: "80%",
+                                    }}
+                                >
+                                    <div className="d-flex align-items-center gap-2">
+                                        <FaUser size={16} style={{ color: "#b3b3b3" }} />
+                                        <span className="m-0" style={{ color: "#b3b3b3" }}>Arianna Salpietro on</span>
+
+                                    </div>
+                                    <p className="m-0" style={{ color: "#b3b3b3" }}>XCC alla BTO 2025: autenticità, dati e innovazione guidano il futuro del travel</p>
+                                </div>
+                            </div>
+
+                            <div style={{ position: "relative", flex: 1 }}>
+                                <Image
+                                    src="/photo6.jpeg"
+                                    alt="foto"
+                                    fill
+                                    style={{ objectFit: "cover" }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
